a1af9d51e72abd1bf07a746126bb4fc9c6dd06e1
Raju Boddula <raju.boddula@gmail.com>
2026-05-07 12:12:27 -0500

    commiting changes

---
+++ BaseCamp1/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from basecamp1!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Week1/Day_1/example.py
@@ -0,0 +1,5 @@
+import code
+x = 42
+y = "Hello Raju"
+code.interact(local=locals())
+print("This will print if you press ctrl+D")
